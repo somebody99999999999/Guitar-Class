03a108a3eb19543def157cff776a0f99b69375eb
Eduardo roman <student.160097@worcesterschools.net>
2026-05-20 13:42:31 -0400

    update

--- Guitar Class/main.py
+++ Guitar Class/main.py
@@ -1,7 +1,11 @@
 class Guitar:
-    def __init__(self, strings, tuner):
+    def __init__(self, guitar_type, strings, tuner):
+        self.guitar_type = guitar_type
         self.strings = strings
         self.tuner = tuner
+
+    def __str__(self):
+        return f"{self.guitar_type} guitar with {self.strings} strings using a {self.tuner} tuner"
     
     def playGuitar(self):
         print("Your now playing the guitar")
@@ -9,9 +13,15 @@ class Guitar:
     def tuningGuitar(self):
         print("Your now tuning the guitar")
 
-    def __str__(self):
-        return f"Guitar with {self.strings} strings uses a {self.tuner} tuner"
+    def play(self):
+        print("")
 
-g = Guitar(3, "normal")
+guitar_type = input("What type of guitar are you using ")
+strings = input("How many strings does it have ")
+tuner = input("What type of tuner are you using ")
 
-print(g)
+g1 = Guitar(guitar_type, strings, tuner)
+
+print(g1)
+
+g1.play()
