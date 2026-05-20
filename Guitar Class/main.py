5b905e7ed68dc4fa6a99de299c651dce871c03b1
Eduardo roman <student.160097@worcesterschools.net>
2026-05-20 12:45:06 -0400

    update

--- Guitar Class/main.py
+++ Guitar Class/main.py
@@ -1,7 +1,11 @@
 class Guitar:
-    def __init__(self, strings, tuner ):
+    def __init__(self, strings, tuner):
         self.strings = strings
         self.tuner = tuner
 
     def __str__(self):
-        return f"{self.strings} is"
+        return f"Guitar with {self.strings} strings uses a {self.tuner} tuner"
+
+g = Guitar(3, "normal")
+
+print(g)
